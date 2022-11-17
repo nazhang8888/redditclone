172177c16e3b8e890bbc1345a3eae11a167c6254
nazhang8888 <nazhang@pm.me>
2022-11-16 22:30:37 -0500

    email logic with thymeleaf

---
+++ src/main/java/com/clone/springredditclone/service/MailService.java
@@ -0,0 +1,33 @@
+package com.clone.springredditclone.service;
+
+import com.clone.springredditclone.exceptions.SpringRedditException;
+import com.clone.springredditclone.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+class MailService {
+        private final sendMail(NotificationEmail notificationEmail) {
+            MimeMessagePreparator messagePreparator = mimeMessage -> {
+                MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+                messageHelper.setFrom("springredditclone@email.com");
+                messageHelper.setTo(notificationEmail.getRecipient());
+                messageHelper.setSubject(notificationEmail.getSubject());
+                messageHelper.setText(notificationEmail.getBody());
+            };
+            try {
+                mailSender.send(messagePreparator);
+                log.info("Activation email sent!");
+                } catch (MailException e) {
+                    log.error("Exception occured when sending mail", e);
+                    throw new SpringRedditException("Exception occurred when sending mail to " + notificationEmail.getRecipient(), e);
+            }
+        }
+}
